a89f94130f20359f9af23f9f65223e25b9c871ae
huaguonave <hua.guo-nave@crossknowledge.com>
2017-11-19 10:41:13 +0100

    ajax elements

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+   url: 'https://www.codeschool.com/users/hguonave.json',
+   dataType: 'jsonp',
+   success: function(response) {
+     $.each(response, function(index, el)
+      {
+        var courses.completed = response.courses.completed;
+        $("#badges").append('<div class="course"></div>');
+        $( ".course" ).append("<h3>"+courses.completed[index].title+"</h3>");
+        $( ".course" ).append('<img src="'+courses.completed[index].badge+'"/>');
+        $( ".course" ).append('<a href="'+courses.completed[index].url+'" target="_blank" class="btn btn-primary">See Course</a>'); 
+      });
+   }
+ });
 
 });
