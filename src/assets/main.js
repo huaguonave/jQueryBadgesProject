814c49492253e26019146ea2f68f9a158daee765
huaguonave <hua.guo-nave@crossknowledge.com>
2017-11-19 11:20:11 +0100

    append in the loop

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,11 +8,7 @@ $(function() {
      var coursesCompleted = response.courses.completed;
      $.each(coursesCompleted, function(index, el)
       {
-        $("#badges").append('<div class="course"></div>');
-        var this = $( ".course" );
-        this.append("<h3>"+el.title+"</h3>");
-        this.append('<img src="'+el.badge+'"/>');
-        this.append('<a href="'+el.url+'" target="_blank" class="btn btn-primary">See Course</a>');
+        $("#badges").append('<div class="course"><h3>'+el.title+'</h3><img src="'+el.badge+'"/><a href="'+el.url+'" target="_blank" class="btn btn-primary">See Course</a></div>');
       });
    }
  });
