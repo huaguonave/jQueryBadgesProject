ae906efbab2a86145e6813a035dc7688ad616c28
huaguonave <hua.guo-nave@crossknowledge.com>
2017-11-19 11:08:47 +0100

    again change

--- src/assets/main.js
+++ src/assets/main.js
@@ -9,7 +9,7 @@ $(function() {
      $.each(coursesCompleted, function(index, el)
       {
         $("#badges").append('<div class="course"></div>');
-        $( ".course" ).html("<h3>"+el.title+"</h3>");
+        $( ".course" ).append("<h3>"+el.title+"</h3>");
         $( ".course" ).append('<img src="'+el.badge+'"/>');
         $( ".course" ).append('<a href="'+el.url+'" target="_blank" class="btn btn-primary">See Course</a>');
       });
