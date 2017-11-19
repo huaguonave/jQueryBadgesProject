18af5dfc55973fd018a9cdaded2b4b76f2e0993d
huaguonave <hua.guo-nave@crossknowledge.com>
2017-11-19 11:14:58 +0100

    test this

--- src/assets/main.js
+++ src/assets/main.js
@@ -9,9 +9,10 @@ $(function() {
      $.each(coursesCompleted, function(index, el)
       {
         $("#badges").append('<div class="course"></div>');
-        $( ".course" ).append("<h3>"+el.title+"</h3>");
-        $( ".course" ).append('<img src="'+el.badge+'"/>');
-        $( ".course" ).append('<a href="'+el.url+'" target="_blank" class="btn btn-primary">See Course</a>');
+        var this = $( ".course" );
+        this.append("<h3>"+el.title+"</h3>");
+        this.append('<img src="'+el.badge+'"/>');
+        this.append('<a href="'+el.url+'" target="_blank" class="btn btn-primary">See Course</a>');
       });
    }
  });
